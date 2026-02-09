9d0ec13e123dcf8a296c0af0f084b62b2693b6cf
PureLearning-dev <liu.jie.coder@gmail.com>
2026-02-09 22:32:09 +0800

    feat: Python-for...else

--- python/1.Python_Basics/9.Python Loop with Else Clause/52.for…else.py
+++ python/1.Python_Basics/9.Python Loop with Else Clause/52.for…else.py
@@ -1,3 +1,8 @@
+# 在for循环语句中有个可选的else语句
+
+# 只有当要进行迭代的对象为空，或者里面没有触发break，才会执行else中的代码
+
+# 不触发break，执行else代码
 people = [{'name': 'John', 'age': 25},
         {'name': 'Jane', 'age': 22},
         {'name': 'Peter', 'age': 30},
@@ -22,3 +27,11 @@ for person in people:
         break
 else:
     print("没找到")
+
+people  = []
+
+# 遍历空字典会执行else语句中的代码
+for person in people:
+    print(person)
+else:
+    print("The list is empty.")
